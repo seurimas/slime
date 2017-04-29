19c62ed4e8efffcee9ac9ab6305d76eed82cd8e4
Seurimas <nicholasanderson5@gmail.com>
2017-04-29 16:57:44 -0500

    Adding a basic pong example and working on the system.

---
+++ examples/pong/src/pong.elm
@@ -0,0 +1,310 @@
+module Pong exposing (main)
+
+{-|
+@docs main
+-}
+
+import Dict exposing (Dict)
+import Html exposing (Html)
+import Color exposing (Color)
+import Char exposing (toCode)
+import AnimationFrame
+import Char exposing (KeyCode)
+import Keyboard.Extra
+import Game.TwoD as Game
+import Game.TwoD.Render as Render
+import Game.TwoD.Camera as Camera exposing (Camera)
+import Slime exposing (..)
+
+
+type alias Rect =
+    { x : Float
+    , y : Float
+    , width : Float
+    , height : Float
+    , color : Color
+    }
+
+
+collide rect1 rect2 =
+    rect1.x
+        + rect1.width
+        > rect2.x
+        && rect1.x
+        < rect2.x
+        + rect2.width
+        && rect1.y
+        + rect1.height
+        > rect2.y
+        && rect1.y
+        < rect2.y
+        + rect2.height
+
+
+type alias ControlSet =
+    { up : KeyCode
+    , down : KeyCode
+    }
+
+
+type alias Ball =
+    { vx : Float, vy : Float }
+
+
+type alias Paddle =
+    { inputState : ( Bool, Bool ), inputScheme : ControlSet }
+
+
+type alias World =
+    EntitySet
+        { transforms : ComponentSet Rect
+        , balls : ComponentSet Ball
+        , paddles : ComponentSet Paddle
+        }
+
+
+type alias Model =
+    { world : World
+    , keys : Keyboard.Extra.Model
+    }
+
+
+type Msg
+    = Tick Float
+    | Keys Keyboard.Extra.Msg
+
+
+transforms : ComponentSpec Rect World
+transforms =
+    { getter = .transforms
+    , setter = (\rec -> \comps -> { rec | transforms = comps })
+    }
+
+
+balls : ComponentSpec Ball World
+balls =
+    { getter = .balls
+    , setter = (\rec -> \comps -> { rec | balls = comps })
+    }
+
+
+spawnBall : ( Float, Float, Float, Float ) -> World -> ( World, Int )
+spawnBall ( x, y, vx, vy ) world =
+    let
+        rect =
+            { x = x
+            , y = y
+            , width = 5
+            , height = 5
+            , color = Color.red
+            }
+
+        ball =
+            { vx = vx
+            , vy = vy
+            }
+    in
+        spawnEntity2 transforms balls world { a = rect, b = ball }
+
+
+paddles : ComponentSpec Paddle World
+paddles =
+    { getter = .paddles
+    , setter = (\rec -> \comps -> { rec | paddles = comps })
+    }
+
+
+spawnPaddles world =
+    let
+        paddleSpawner =
+            spawnEntity2 transforms paddles
+
+        leftRect =
+            { x = 10
+            , y = 250
+            , width = 10
+            , height = 50
+            , color = Color.green
+            }
+
+        rightRect =
+            { x = 480
+            , y = 250
+            , width = 10
+            , height = 50
+            , color = Color.green
+            }
+
+        leftInput =
+            { inputState = ( False, False )
+            , inputScheme = { up = toCode 'W', down = toCode 'S' }
+            }
+
+        rightInput =
+            { inputState = ( False, False )
+            , inputScheme = { up = toCode 'I', down = toCode 'K' }
+            }
+    in
+        paddleSpawner world { a = leftRect, b = leftInput }
+            |> Tuple.first
+            >> flip paddleSpawner { a = rightRect, b = rightInput }
+            |> Tuple.first
+
+
+world : World
+world =
+    { idSource = Slime.initIdSource
+    , transforms = initComponents
+    , balls = initComponents
+    , paddles = initComponents
+    }
+        |> spawnBall ( 247.5, 247.5, 100, 80 )
+        |> Tuple.first
+        |> spawnBall ( 247.5, 247.5, 80, 100 )
+        |> Tuple.first
+        |> spawnBall ( 247.5, 247.5, -100, 80 )
+        |> Tuple.first
+        |> spawnBall ( 247.5, 247.5, -80, 100 )
+        |> Tuple.first
+        |> spawnBall ( 247.5, 247.5, 100, -80 )
+        |> Tuple.first
+        |> spawnBall ( 247.5, 247.5, 80, -100 )
+        |> Tuple.first
+        |> spawnBall ( 247.5, 247.5, -100, -80 )
+        |> Tuple.first
+        |> spawnBall ( 247.5, 247.5, -80, -100 )
+        |> Tuple.first
+        |> spawnBall ( 40, 230, -80, 100 )
+        |> Tuple.first
+        |> spawnPaddles
+
+
+model : Model
+model =
+    { world = world
+    , keys = Tuple.first Keyboard.Extra.init
+    }
+
+
+moveBalls : Float -> World -> World
+moveBalls delta =
+    let
+        addVelocity ball transform delta =
+            { transform
+                | x = transform.x + ball.vx * delta
+                , y = transform.y + ball.vy * delta
+            }
+    in
+        stepEntities2 balls transforms (\ent2 -> { ent2 | b = addVelocity ent2.a ent2.b delta })
+
+
+keepBalls : World -> World
+keepBalls =
+    let
+        maybeBounce e2 =
+            let
+                ball =
+                    e2.a
+
+                transform =
+                    e2.b
+            in
+                if transform.x + transform.width >= 500 && ball.vx > 0 then
+                    { e2 | a = { ball | vx = -ball.vx }, b = { transform | x = 500 - transform.width } }
+                else if transform.x <= 0 && ball.vx < 0 then
+                    { e2 | a = { ball | vx = -ball.vx }, b = { transform | x = 0 } }
+                else if transform.y + transform.height >= 500 && ball.vy > 0 then
+                    { e2 | a = { ball | vy = -ball.vy }, b = { transform | y = 500 - transform.height } }
+                else if transform.y <= 0 && ball.vy < 0 then
+                    { e2 | a = { ball | vy = -ball.vy }, b = { transform | y = 0 } }
+                else
+                    e2
+    in
+        stepEntities2 balls transforms maybeBounce
+
+
+bounceBalls : World -> World
+bounceBalls =
+    let
+        bounce e1 e2 =
+            let
+                paddle =
+                    e1.b
+
+                ballVel =
+                    e2.a
+
+                ballPos =
+                    e2.b
+            in
+                if collide ballPos paddle then
+                    if (ballVel.vx < 0 && ballPos.x < paddle.x + paddle.width) then
+                        { e2 | a = { ballVel | vx = -1 * ballVel.vx }, b = { ballPos | x = paddle.x + paddle.width } }
+                    else if (ballVel.vx > 0 && ballPos.x + ballPos.width > paddle.x) then
+                        { e2 | a = { ballVel | vx = -1 * ballVel.vx }, b = { ballPos | x = paddle.x - ballPos.width } }
+                    else
+                        e2
+                else
+                    e2
+
+        paddleList =
+            entities2 paddles transforms world
+    in
+        stepEntities2 balls transforms (\e2 -> List.foldr bounce e2 paddleList)
+
+
+renderTransforms =
+    Slime.map transforms
+        (\transform ->
+            Render.rectangle
+                { color = transform.color
+                , position = ( transform.x, transform.y )
+                , size = ( transform.width, transform.height )
+                }
+        )
+
+
+render { world } =
+    Game.render
+        { time = 0
+        , camera = Camera.fixedHeight 500 ( 250, 250 )
+        , size = ( 500, 500 )
+        }
+        (renderTransforms world)
+
+
+subs m =
+    Sub.batch
+        [ Sub.map Keys Keyboard.Extra.subscriptions
+        , AnimationFrame.diffs Tick
+        ]
+
+
+update msg model =
+    case msg of
+        Tick delta ->
+            let
+                deltaMs =
+                    delta / 1000
+            in
+                { model | world = model.world |> moveBalls deltaMs |> keepBalls |> bounceBalls } ! []
+
+        Keys kMsg ->
+            let
+                ( kModel, kCmd ) =
+                    Keyboard.Extra.update kMsg model.keys
+            in
+                { model | keys = kModel } ! [ Cmd.map Keys kCmd ]
+
+
+{-|
+   The game
+-}
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = model ! []
+        , subscriptions = subs
+        , update = update
+        , view = render
+        }
--- src/slime.elm
+++ src/slime.elm
@@ -1,23 +1,29 @@
-module Slime exposing (EntitySet, ComponentSpec, EntitySetter, EntitySetter2, ComponentDeletor, deletor, (&&), Entity, Entity2, Entity3, spawnEntity, setEntity, setEntity2, entities, entities2, getComponent)
+module Slime exposing (EntitySet, ComponentSpec, ComponentSet, EntitySetter, EntitySetter2, ComponentDeletor, initComponents, initIdSource, deletor, (&>), Entity, Entity2, Entity3, spawnEntity, spawnEntity2, setEntity, setEntity2, entities, entities2, getComponent, map, stepComponents, stepEntities, stepEntities2)
 
 {-| This library provides an easy way to construct entity-component-system style
 codeflow in Elm.
 
 # Types
-@docs EntitySet, ComponentSpec, EntitySetter, EntitySetter2, ComponentDeletor, Entity, Entity2, Entity3
+@docs EntitySet, ComponentSpec, ComponentSet, EntitySetter, EntitySetter2, EntitySetter3, ComponentDeletor, Entity, Entity2, Entity3
+
+# Updates and Maps
+@docs map, stepComponents, stepEntities, stepEntities2
+
+# Initialization
+@docs init
 
 # Deletion
-@docs deletor, (&&)
+@docs deletor, (&>)
 
 # Retrieval
 @docs entities, entities2, getComponent
 
 # Creation and setting
-@docs setEntity, setEntity2, spawnEntity
+@docs setEntity, setEntity2, spawnEmpty, spawnEntity, spawnEntity2
 -}
 
 import Array exposing (Array, length, append, push, get, set, repeat, indexedMap, toList)
-import Array.Extra exposing (zip, filterMap)
+import Array.Extra exposing (zip, zip3, filterMap)
 import Lazy.List exposing (LazyList, numbers, drop, (+++), headAndTail)
 
 
@@ -34,6 +40,11 @@ type alias ComponentDeletor rec =
 
 
 {-| -}
+type alias SystemStep rec =
+    rec -> rec
+
+
+{-| -}
 type alias EntitySetter a rec =
     rec -> Entity a -> rec
 
@@ -44,10 +55,14 @@ type alias EntitySetter2 a b rec =
 
 
 {-| -}
+type alias EntitySetter3 a b c rec =
+    rec -> Entity3 a b c -> rec
+
+
+{-| -}
 type alias EntitySet rec =
     { rec
-        | deletors : List (ComponentDeletor rec)
-        , idSource : LazyList Int
+        | idSource : LazyList Int
     }
 
 
@@ -66,29 +81,12 @@ type alias Entity2 a b =
     }
 
 
-addB : b -> Entity a -> Entity2 a b
-addB b e =
-    { id = e.id
-    , a = e.a
-    , b = b
-    }
-
-
 {-| -}
 type alias Entity3 a b c =
     { id : Int
     , a : a
     , b : b
     , c : c
-    }
-
-
-addC : c -> Entity2 a b -> Entity3 a b c
-addC c e =
-    { id = e.id
-    , a = e.a
-    , b = e.b
-    , c = c
     }
 
 
@@ -108,16 +106,48 @@ tag i ma =
             Nothing
 
 
+addB : b -> Entity a -> Entity2 a b
+addB b e =
+    { id = e.id
+    , a = e.a
+    , b = b
+    }
+
+
 tag2 : Int -> ( Maybe a, Maybe b ) -> Maybe (Entity2 a b)
 tag2 i ( ma, mb ) =
     tag i ma
         |> Maybe.map2 addB mb
 
 
+addC : c -> Entity2 a b -> Entity3 a b c
+addC c e =
+    { id = e.id
+    , a = e.a
+    , b = e.b
+    , c = c
+    }
+
+
 tag3 : Int -> ( Maybe a, Maybe b, Maybe c ) -> Maybe (Entity3 a b c)
 tag3 i ( ma, mb, mc ) =
     tag2 i ( ma, mb )
         |> Maybe.map2 addC mc
+
+
+
+{-
+   Initialization
+-}
+
+
+initComponents : ComponentSet a
+initComponents =
+    { contents = Array.empty }
+
+
+initIdSource =
+    numbers
 
 
 
@@ -136,7 +166,7 @@ deleteComponent index components =
 
 
 {-| -}
-deletor : ComponentSpec a rec -> ComponentDeletor rec
+deletor : ComponentSpec a (EntitySet rec) -> ComponentDeletor (EntitySet rec)
 deletor { getter, setter } record id =
     getter record
         |> deleteComponent id
@@ -144,8 +174,8 @@ deletor { getter, setter } record id =
 
 
 {-| -}
-(&&) : ComponentDeletor rec -> ComponentDeletor rec -> ComponentDeletor rec
-(&&) first second record index =
+(&>) : ComponentDeletor rec -> ComponentDeletor rec -> ComponentDeletor rec
+(&>) first second record index =
     first record index
         |> flip second index
 
@@ -157,8 +187,8 @@ deletor { getter, setter } record id =
 
 
 {-| -}
-spawnEntity : EntitySet rec -> ( EntitySet rec, Int )
-spawnEntity entitySet =
+spawnEmpty : EntitySet rec -> ( EntitySet rec, Int )
+spawnEmpty entitySet =
     let
         alwaysPresent =
             headAndTail entitySet.idSource
@@ -171,17 +201,49 @@ spawnEntity entitySet =
                 ( entitySet, 0 )
 
 
+spawnEntity : ComponentSpec a (EntitySet rec) -> EntitySet rec -> { a : a } -> ( EntitySet rec, Int )
+spawnEntity { getter, setter } entitySet { a } =
+    let
+        ( updatedSet, id ) =
+            spawnEmpty entitySet
+
+        updatedComponents =
+            getter updatedSet
+                |> setComponent id a
+    in
+        ( setter updatedSet updatedComponents, id )
+
+
+spawnEntity2 : ComponentSpec a (EntitySet rec) -> ComponentSpec b (EntitySet rec) -> EntitySet rec -> { a : a, b : b } -> ( EntitySet rec, Int )
+spawnEntity2 specA specB entitySet { a, b } =
+    let
+        ( updatedSet, id ) =
+            spawnEmpty entitySet
+
+        updatedComponentsA =
+            specA.getter updatedSet
+                |> setComponent id a
+
+        updatedComponentsB =
+            specB.getter updatedSet
+                |> setComponent id b
+    in
+        ( specA.setter updatedSet updatedComponentsA |> flip specB.setter updatedComponentsB, id )
+
+
 setComponent : Int -> a -> ComponentSet a -> ComponentSet a
 setComponent index value components =
-    let
-        missing =
-            index - length components.contents
-
-        newContents =
-            append components.contents (repeat missing Nothing)
-                |> push (Just value)
-    in
-        { components | contents = newContents }
+    if index - length components.contents < 0 then
+        { components
+            | contents =
+                set index (Just value) components.contents
+        }
+    else
+        { components
+            | contents =
+                append components.contents (repeat (index - length components.contents) Nothing)
+                    |> push (Just value)
+        }
 
 
 {-| Useful for time-based components which simply need to be updated incrementally
@@ -193,6 +255,54 @@ stepComponents update components =
             Array.map (Maybe.map update) components.contents
     in
         { components | contents = newContents }
+
+
+{-|
+-}
+stepEntities : ComponentSpec a rec -> (a -> a) -> rec -> rec
+stepEntities { getter, setter } update record =
+    let
+        components =
+            getter record
+
+        updatedComponents =
+            stepComponents update components
+    in
+        setter record updatedComponents
+
+
+{-|
+-}
+stepEntities2 : ComponentSpec a rec -> ComponentSpec b rec -> (Entity2 a b -> Entity2 a b) -> rec -> rec
+stepEntities2 specA specB update record =
+    let
+        entities =
+            entities2 specA specB record
+
+        updatedEntities =
+            List.map update entities
+
+        setter =
+            setEntity2 specA specB
+    in
+        (List.foldr (flip setter) record updatedEntities)
+
+
+{-|
+-}
+stepEntities3 : ComponentSpec a rec -> ComponentSpec b rec -> ComponentSpec c rec -> (Entity3 a b c -> Entity3 a b c) -> rec -> rec
+stepEntities3 specA specB specC update record =
+    let
+        entities =
+            entities3 specA specB specC record
+
+        updatedEntities =
+            List.map update entities
+
+        setter =
+            setEntity3 specA specB specC
+    in
+        (List.foldr (flip setter) record updatedEntities)
 
 
 {-| -}
@@ -222,6 +332,27 @@ setEntity2 specA specB record entity =
             |> flip specB.setter updatedComponentsB
 
 
+{-| -}
+setEntity3 : ComponentSpec a rec -> ComponentSpec b rec -> ComponentSpec c rec -> EntitySetter3 a b c rec
+setEntity3 specA specB specC record entity =
+    let
+        updatedComponentsA =
+            specA.getter record
+                |> setComponent entity.id entity.a
+
+        updatedComponentsB =
+            specB.getter record
+                |> setComponent entity.id entity.b
+
+        updatedComponentsC =
+            specC.getter record
+                |> setComponent entity.id entity.c
+    in
+        specA.setter record updatedComponentsA
+            |> flip specB.setter updatedComponentsB
+            |> flip specC.setter updatedComponentsC
+
+
 
 {-
    Retrieval
@@ -241,6 +372,16 @@ completeEntities =
 
 
 {-| -}
+map : ComponentSpec a rec -> (a -> b) -> rec -> List b
+map { getter } func record =
+    getter record
+        |> .contents
+        |> filterMap identity
+        |> toList
+        |> List.map func
+
+
+{-| -}
 entities : ComponentSpec a rec -> rec -> List (Entity a)
 entities { getter } record =
     getter record
@@ -254,4 +395,12 @@ entities2 : ComponentSpec a rec -> ComponentSpec b rec -> rec -> List (Entity2 a
 entities2 specA specB record =
     zip ((.contents << specA.getter) record) ((.contents << specB.getter) record)
         |> indexedMap tag2
+        |> completeEntities
+
+
+{-| -}
+entities3 : ComponentSpec a rec -> ComponentSpec b rec -> ComponentSpec c rec -> rec -> List (Entity3 a b c)
+entities3 specA specB specC record =
+    zip3 ((.contents << specA.getter) record) ((.contents << specB.getter) record) ((.contents << specC.getter) record)
+        |> indexedMap tag3
         |> completeEntities
